30fd571c18f2d70aecc2d71e5a5023e0fadf6e6a
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-10-11 12:03:33 -0500

    added-compo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import CardFlip from './compo/Card-Flip';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <CardFlip/>
     </div>
   );
 }
---
+++ src/compo/Card-Flip.js
@@ -0,0 +1,27 @@
+import React,{useState} from 'react';
+import ReactCardFlip from 'react-card-flip';
+
+
+const CardFlip = () => {
+    const [isFlipped , setIsFlipped] = useState(false)
+
+    const handleClick = () => {
+        setIsFlipped( !isFlipped   )
+        }
+
+  return (
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+    <div className ='front'>
+      This is the front of the card.
+      <button onClick={handleClick}>Click to flip</button>
+    </div>
+
+    <div className ='back'>
+      This is the back of the card.
+      <button onClick={handleClick}>Click to flip</button>
+    </div>
+  </ReactCardFlip>
+  );
+}
+
+export default CardFlip;
